b455eda269d2f4c21890a3398fc3b294b4093830
Crea8ive Dev <crea8ivedev@gmail.com>
2025-07-11 22:50:34 +0530

    debugging

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,16 @@
-import { defineConfig, loadEnv } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig(({ command, mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-  return {
-  plugins: [react(), tailwindcss()],
- server: {
-  host: "0.0.0.0",
-  proxy: {
-    '/api': {
-      target: env.VITE_API_URL,
-      changeOrigin: true,
-      secure: env.VITE_NODE_ENV === "production",
-      ws: true,
-    }
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(),tailwindcss()],
+  base : "/",
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    historyApiFallback: true, // for dev
   }
-},
-}
+  
 })
-
